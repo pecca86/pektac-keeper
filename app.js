2319d6c0801dda5d47391783018e4b0eee78d69c
Pekka Ranta-aho <pekka.rantaaho86@gmail.com>
2021-05-16 21:49:29 +0300

    ready for heroku deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // === EXPRESS ==
 const express = require("express");
 const app = express();
+const path = require("path");
 
 // === COOKIES ===
 const cookieParser = require("cookie-parser");
@@ -18,7 +19,6 @@ connectDB();
 // Parses JSON body data
 app.use(express.json({ extended: false }));
 
-
 // === ROUTES IMPORT ===
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
@@ -28,11 +28,21 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactsRoutes);
 
-
 // Errorhandling middleware for routes
 // NEEDS TO COME AFTER ALL OTHER MIDDLEWARE!
 const errorHandler = require("./middleware/error");
 app.use(errorHandler);
+
+// Serve static assets in production (REACT):
+if (process.env.NODE_ENV === "production") {
+  // set static REACT folder
+  app.use(express.static("client/build"));
+  // get any URL (IMPORTAN! has to come AFTER all other routes)
+  // Searches for the REACT index.html
+  app.get("*", (res, req) =>
+    res.send(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // === LISTEN TO PORT ON SERVER ===
 const PORT = process.env.PORT || 5000;
